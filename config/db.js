be5fdac86d1ccd7216235844656cab12bd5b02d5
yosephfetene <155078686+yosephfetene@users.noreply.github.com>
2026-06-14 11:25:54 -0700

    db

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle PostgreSQL client', err);
+});
+
+module.exports = pool;
